dbda83b62c2f7660b55de18a93131c7bd727c22a
Donny2g <donny2ghu@Donny2gdeiMac.local>
2015-03-30 10:03:34 +0800

    Initial Commit

---
+++ OAuthDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OAuthDemo
+//
+//  Created by Donny2g Hu on 15/3/30.
+//  Copyright (c) 2015年 Donny2g Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
